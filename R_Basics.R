975adb6f6bdb05e1f47ed58ab5dab513c00cc404
victorpaolo <51170664+victorpaolo@users.noreply.github.com>
2022-01-24 23:13:43 +0100

    Update R_Basics.R

--- R_Basics.R
+++ R_Basics.R
@@ -367,45 +367,6 @@ str(airquality)
 table(airquality) ##better with a vector or a column
 
 
-###############################################################################
-#Charts
-##Boxplot
-help("boxplot")
-### By default
-boxplot(iris)
-### By formula
-boxplot(iris, range = 1.5, width = NULL, varwidth = FALSE,
-        notch = FALSE, outline = TRUE, names, plot = TRUE,
-        border = par("fg"), col = "lightgray", log = "",
-        pars = list(boxwex = 1.5, staplewex = 0.5, outwex = 0.5), 
-        horizontal = TRUE, add = TRUE, at = NULL)
-
-##Plot
-help("plot")
-plot(iris)
-plot(Sepal.Length ~ Petal.Length, data=iris)
-plot(iris, y = NULL,  xlim = NULL, ylim = NULL,
-     log = "", main = NULL, sub = NULL,
-     ann = par("ann"),
-     panel.first = NULL, panel.last = NULL, asp = NA,
-     xgap.axis = NA, ygap.axis = NA)
-
-
-##Barplot
-
-##Histogram
-
-##Pie
-
-##Save a chart
-
-##Ggplot
-
-##Maps
-
-##Highcharter
-
-
 
 
 ###############################################################################
